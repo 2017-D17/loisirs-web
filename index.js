f46ad51246a1412b1c248ac73e8b1c3d6854a0f0
Rossi Oddet <rossi.oddet@dev-institut.fr>
2017-11-28 11:24:32 +0100

    affichage de l'équipe

--- index.js
+++ index.js
@@ -1,40 +1,23 @@
 var githubUsers = ['Melodie44', 'AlexGeb', 'Tagpower', 'thienban', 'Kazekitai', 'AssiaTrabelsi', 'MAWAAW', 'myR3po'];
 
-var templateCard = `
-<div class="ui card">
-    <div class="image">
-    <img src="{{avatar_url}}">
-    </div>
-    <div class="content">
-    <a class="header">{{login}}</a>
-    <div class="meta">
-        <span class="date">Compte créé le {{created_at}}</span>
-    </div>
-    <div class="description">
-        Tu as {{public_repos}} dépôts publiques. 
-    </div>
-    </div>
-    <div class="extra content">
-    <a>
-        <i class="user icon"></i>
-        {{followers}} Amis
-    </a>
-    </div>
-</div>
 
+$.get('partials/cardUser.html').then(function (templateCard) {
+    Promise.all(
+        githubUsers.map(function (userId) {
+            return $.get('https://api.github.com/users/' + userId)
+        })
+    ).then(function (users) {
+        var body = ''
+        users.forEach(function (user) {
+            console.log(user)
+            body += templateCard
+                .replace('{{avatar_url}}', user.avatar_url)
+                .replace('{{login}}', user.login)
+                .replace('{{public_repos}}', user.public_repos)
+                .replace('{{created_at}}', new Date(user.created_at).getFullYear())
+                .replace('{{followers}}', user.followers)
 
-`
-var body = ''
-
-githubUsers.forEach(function(userId){
-    $.get('https://api.github.com/users/'+userId, function(user){
-        console.log(user)
-        body+= templateCard
-                    .replace('{{avatar_url}}', user.avatar_url)
-                    .replace('{{login}}', user.login)
-                    .replace('{{public_repos}}', user.public_repos)
-                    .replace('{{created_at}}', user.created_at)                 
-                    .replace('{{followers}}', user.followers)
+        })
         document.querySelector('main').innerHTML = body
     })
 })
