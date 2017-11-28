a0a6a9a8a9293ec092909af228ce14bffc3a9e36
AlexGeb <alex.behaghel@gmail.com>
2017-11-28 12:03:18 +0100

    added base url

--- escalade/script.js
+++ escalade/script.js
@@ -1,19 +1,26 @@
 $(document).ready(function () {
+
+    let navHeight = $('nav').height();
+    $('body').css('padding-top', + navHeight + 15 + 'px');
+    $(window).resize(function () {
+        navHeight = $('nav').height();
+        $('body').css('padding-top', + navHeight + 15 + 'px');
+    });
+
+
     $('body').scrollspy({ target: '#nav-escalade' })
 
     $(".navbar ul li a[href^='#']").not('[href="#"]').on('click', function (e) {
         // prevent default anchor click behavior
         e.preventDefault();
         // store hash
-        var hash = this.hash;
+        let hash = this.hash;
         console.log(hash)
         // animate
         $('html, body').animate({
-            scrollTop: $(this.hash).offset().top - 75
+            scrollTop: $(this.hash).offset().top - navHeight - 15
         }, 300, function () {
             // when done, add hash to url
-            // (default click behaviour)
-
             window.location.hash = hash;
         });
 
