550a16bba08b35baf35608c3f4ddc41726229fec
Asia Trabelsi <assia.trabelsi93@gmail.com>
2017-11-29 15:10:09 +0100

    inscription fini

---
+++ formulaire/form.js
@@ -0,0 +1,67 @@
+
+       function register(event) {
+                event.preventDefault();    
+
+                console.log("inscription en cours")
+            
+                // Get some values from elements on the page:
+            
+                var $form = $(this);
+            
+                var username = document.getElementById("username1").value;
+            
+                var password = document.getElementById("password1").value;
+            
+                console.log("name = " + username + " et passs = " + password)
+            
+                // Send the data using post
+            
+                var url = "http://loisirs-web-backend.cleverapps.io/users";
+            
+                $.post(url, { "name": username, "password": password }).then(resp => {
+            
+                    console.log(resp)
+            
+                })
+
+
+
+
+                jQuery(function(e){ 
+                     e.preventDefault();
+                            var username = $("#username");
+                            valid = false; 
+                    /* je verifie que le champs n'est pas vide et j'applique une classe et j'affiche la div error*/
+                            $("#username").blur(function(){
+                                                    if($("#username").val() == ""){
+                                                            nom.next(".error").show().text("Le champ username  est obligatoire "); 
+                                                            
+                                                            valid = false;
+                                                    }
+                                                    else { valid = true }
+                            }); 
+                            var password=$("#password") ;
+                            $("#password").blur(function(){
+                                if($("#password").val() == ""){
+                                        nom.next(".error").show().text("Le champ password  est obligatoire "); 
+                                      
+                                        valid = false;
+                                }
+                                else { valid = true }
+                }); 
+                
+                            $("#button").click(function(){
+                                if(valid == true)
+           
+                                location.href = "./voyage/index.html";
+                                        
+                                else
+
+                                alert("les champs username et password sont obligatoires")
+                                        
+                        });
+                
+                    });
+                }
+
+            
