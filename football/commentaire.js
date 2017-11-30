cee62beff502e57b74cc62e081b5e636da31568a
thien ban BUI <thienban91@hotmail.fr>
2017-11-30 17:44:18 +0100

    commentaire3

--- football/commentaire.js
+++ football/commentaire.js
@@ -1,4 +1,14 @@
 function getwords() {
-    localStorage.setItem("words", document.getElementById("words").value);
+    var commentaire = {
+        message: document.getElementById("words").value,
+        user: getCookie("userName"),
+        date: new Date()
+    }
+
+    var commentaireSTR = localStorage.getItem("message");
+    var commentaire = [];
+
+
+
     document.getElementById("comments").innerHTML = localStorage.getItem("words");
 }
