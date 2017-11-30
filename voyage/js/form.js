49cfec4f47eaafd5fe52856402a52754ad65d470
Asia Trabelsi <assia.trabelsi93@gmail.com>
2017-11-30 15:32:06 +0100

    ajout des commentaires

--- voyage/js/form.js
+++ voyage/js/form.js
@@ -62,7 +62,36 @@ jQuery(function () {
 
     });
 
+
 })
 
+function setCookie(cname, cvalue) {
+    var d = new Date();
+    d.setTime(d.getTime() + (24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";path=/voyage/";
+}
 
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var connected = getCookie("userConnected");
+    if (!connected) {
+        window.location.replace("http://127.0.0.1:8080/voyage");
+    }
+}
 
