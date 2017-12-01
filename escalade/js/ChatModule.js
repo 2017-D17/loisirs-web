c1eba32f067e3e4f83fada598080fa9da0c9573e
AlexGeb <alex.behaghel@gmail.com>
2017-12-01 13:04:01 +0100

    ChatModule opérationnel

--- escalade/js/ChatModule.js
+++ escalade/js/ChatModule.js
@@ -2,8 +2,80 @@ var App = App || {};
 console.log('App in ChatModule :', App)
 
 App.ChatModule = (function () {
+    var messageSocket = null;
+    var WEBSOCKET_URL_PROD = 'wss://d17chat.cleverapps.io/'
+    var WEBSOCKET_URL_DEV = 'ws://localhost:8080'
+    var username = localStorage.getItem("username")
+    var messages = [];
     var start = function () {
+        username = localStorage.getItem("username")
+        messageSocket = new WebSocket(WEBSOCKET_URL_DEV)
+        messageSocket.onopen = function (event) {
+            console.log('messageSocket open')
+            registerModalEvents()
+        };
 
+        messageSocket.onmessage = (msg) => {
+            messages.push(getJsonMsg(msg.data))
+            addMessageToModal()
+        };
+
+        messageSocket.onclose = (msg) => {
+            console.log('socket closed')
+        };
+        $('#messagerieModal').on('show.bs.modal', function (event) {
+            scrollDownTheModal()
+        })
+    }
+    var registerModalEvents = function () {
+        var modal = $('#messagerieModal')
+        msgSections = modal.find('#messages_section')
+        msgSections.scrollTop(msgSections[0].scrollHeight);
+        modal.find('#inputMessage').on("input propertychange", function (event) {
+            ($(this).val() === "") ? modal.find("#sendMsg").prop("disabled", true) : modal.find("#sendMsg").prop("disabled", false);
+        })
+        modal.find('#sendMsg').on('click', function (event) {
+            console.log('envoi du message', modal.find('#inputMessage').val())
+            messageSocket.send(buildMsg(modal.find('#inputMessage').val()))
+        })
+    }
+    var addMessageToModal = function (data) {
+        /** on vide la zone */
+        $('#messages_section').empty()
+        /** on trie nos messages */
+        messages.sort((msg1, msg2) => {
+            return new Date(msg2.date) - new Date(msg1.date)
+        })
+        /** puis on ajoute les messages au modal dans le bon ordre : */
+
+        messages.forEach(msg => {
+            var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', 'second': '2-digit' };
+            var formattedDate = new Date(msg.date).toLocaleDateString('fr-FR', options)
+            $('#messages_section').prepend(
+                `<div class="list-group-item list-group-item-action flex-column align-items-start">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1">`+ msg.name + `</h5>
+                            <small>`+ formattedDate + `</small>
+                        </div>
+                    <p class="mb-1">`+ msg.message + `</p>
+                    </div>`
+            )
+        })
+        /** scroll to the bottom */
+        scrollDownTheModal()
+    }
+
+    var scrollDownTheModal = function () {
+        var wtf = $('#messages_section');
+        var height = wtf[0].scrollHeight;
+        wtf.scrollTop(height);
+    }
+
+    var buildMsg = function (body) {
+        return JSON.stringify({ name: username, message: body })
+    }
+    var getJsonMsg = function (jsonstring) {
+        return JSON.parse(jsonstring)
     }
     return { start }
 })()
---
+++ escalade/js/LoginFormModule.js
@@ -0,0 +1,10 @@
+var App = App || {};
+console.log('App in CommentsModule :', App)
+
+App.LoginFormModule = (function () {
+    var start = function () {
+
+    }
+
+    return {}
+})()
