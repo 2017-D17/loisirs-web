b37efa863a1399eed96b332ee841d5b4850f2209
thien ban BUI <thienban91@hotmail.fr>
2017-12-01 16:27:23 +0100

    module account connexion

--- football/account.js
+++ football/account.js
@@ -1,6 +1,12 @@
-var Account = (function () {
-    var self = {};
-    self.connexion = function () {
+var account = (function () {
+
+
+    $('#connexionButton').on('click', (event) => {
+        connexion()
+    });
+
+
+    var connexion = function () {
         console.log("connexion en cours")
         // Get some values from elements on the page:
         var nom = document.getElementById("inputName1").value;
@@ -27,12 +33,42 @@ var Account = (function () {
 
         })
     }
-    self.deconnexion = function () {
+    $('#deconnexionButton').on('click', (event) => {
+        deconnexion()
+    });
+    var deconnexion = function () {
         setCookie("username", "", -1);
         location = "index.html";
     }
+    function inscription() {
+        console.log("inscription en cours")
+        // Get username and password
+        var nom = document.getElementById("inputName").value;
+        var motpass = document.getElementById("inputPassword").value;
+        console.log("name = " + nom + " et passs = " + motpass)
+        //url 
+        var url = "https://loisirs-web-backend.cleverapps.io/users";
+        var url1 = "https://loisirs-web-backend.cleverapps.io/users/?name=" + nom;
+        //Vérifier si utilisateur est déjà inscrit
+        $.get(url1).then(response => {
+            if (response.length > 0) {
+                alert("Vous êtes déjà inscrit")
+            }
+            //mettre les données 
+            else {
+                $.post(url, { "name": nom, "password": motpass }).then(resp => {
+                    console.log(resp)
+                    setCookie("username", nom, 1)
+                    location.href = "home.html"
+                    alert("Vous êtes inscrit")
+                })
 
-    return self;
+            }
+
+        })
+
+    }
+    return { connexion, deconnexion, inscription }
 
 })();
 
